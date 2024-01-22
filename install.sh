ab995e06ee05180c3e82226253ac1e24aa471d36
taitep <taitep@taitep.se>
2024-01-22 15:32:42 +0100

    Install AUR helpers

--- install.sh
+++ install.sh
@@ -8,3 +8,20 @@ fi
 
 mkdir ./tmp
 
+sudo pacman -S --needed git base-devel pacman-contrib
+
+# Install AME AUR helper if rust is installed
+cd tmp
+if command -v rustup &> /dev/null; then
+	echo "found rustup, installing ame"
+	git clone https://git.getcryt.al/crystal/pkgbuilds/ame
+	cd ame && makepkg -si && cd ..
+else
+	echo "rustup is not installed, not installing ame"
+fi
+
+echo "installing yay"
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git && makepkg -si && cd ..
+
+cd ..
